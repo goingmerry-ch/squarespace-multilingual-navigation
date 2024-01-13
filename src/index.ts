5ac6db7e18812acc47e21ef936f8fb21b1a65843
upepo mwindaji <upepo.mwindaji@gmail.com>
2024-01-13 15:28:12 +0100

    fix: styling overrides and logo link

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import MlNav from "./mlnav";
-import type { BaseNavigation } from "./mlnav";
+import type { BaseNavigation, HomeNavigation } from "./mlnav";
 
 declare global {
   interface Window {
     _MLNAV_BASE_NAVIGATION: BaseNavigation
+    _MLNAV_HOME: HomeNavigation
   }
 }
 
 if (window._MLNAV_BASE_NAVIGATION) {
-  const mlNav = new MlNav(window._MLNAV_BASE_NAVIGATION);
+  const mlNav = new MlNav(window._MLNAV_BASE_NAVIGATION, window._MLNAV_HOME);
   mlNav.renderNav();
 }
